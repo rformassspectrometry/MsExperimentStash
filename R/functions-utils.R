1441603a4b4a7abdf2686f4c51e3c4064230ba89
Johannes Rainer <johannes.rainer@gmail.com>
2026-06-29 11:34:04 +0200

    feat: add text-file-based MsExperimentFiles stash
    - Add the plain text file-based stash for MsExperimentFiles.

--- R/functions-utils.R
+++ R/functions-utils.R
@@ -11,3 +11,17 @@
         stop("file(s) ", paste0("\"", expected[miss], "\"", collapse = ", "),
              " not found in ", path, call. = FALSE)
 }
+
+#' Check if the file `x` already exists and throw an error if that's TRUE
+#'
+#' Used in:
+#' - *R/MsExperimentFiles.R*: `saveMsObject()`
+#'
+#' @noRd
+.check_overwriting <- function(x) {
+    if (file.exists(x))
+        stop("The provided path contains already an MS object stash. ",
+             "Overwriting an existing stash is not supported. Please remove ",
+             "the directory defined with parameter 'path' first.",
+             call. = FALSE)
+}
--- tests/testthat/test_MsExperimentFiles.R
+++ tests/testthat/test_MsExperimentFiles.R
@@ -35,3 +35,27 @@ test_that("saveMsObject/readMsObject,MsExperimentFiles,AlabasterParam works", {
 
     unlink(d, recursive = TRUE)
 })
+
+test_that("saveMsObject/readMsObject,MsExperimentFiles,PlainTextParam works", {
+    d <- file.path(tempdir(), "test")
+
+    p <- PlainTextParam(d)
+    expect_error(readMsObject(MsExperimentFiles(), p), "not found in")
+
+    a <- MsExperimentFiles()
+    expect_no_error(saveMsObject(a, p))
+    expect_true("ms_experiment_files.txt" %in% dir(d))
+    res <- readMsObject(MsExperimentFiles(), p)
+    expect_equal(a, res)
+
+    a <- MsExperimentFiles(list(library = c("gnps.mgf", "hmdb.mgf"),
+                                input = c("test.mgf")))
+    expect_error(saveMsObject(a, p), "provided path contains")
+
+    unlink(d, recursive = TRUE)
+    expect_no_error(saveMsObject(a, p))
+    expect_no_error(res <- readMsObject(a, p))
+    expect_equal(a, res)
+
+    unlink(d, recursive = TRUE)
+})
